b488a2b3362db046b2a4d2cc4af3388999914b0d
myari211 <appura16@gmail.com>
2021-03-22 12:08:20 +0700

    testing

--- resources/views/master/css.blade.php
+++ resources/views/master/css.blade.php
@@ -8,10 +8,11 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400&display=swap" rel="stylesheet">
-{{-- <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}"> --}}
+<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/icon.ico') }}">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('css/yprsa.css') }}" type="text/css">
+{{-- <link rel="stylesheet" href="{{ asset('css/yprsa.css') }}" type="text/css"> --}}
 <meta name="viewport" content="width=device-width, initial-scale=1.0">  
 <title>Yayasan Pendidikan Sakha Ramdan Aditya</title>
